75e4bf60970b9bc8f577e3123a943b1ed3b9500e
Batistadriana <batistadriana@hotmail.com>
2019-05-10 13:13:07 -0500

    Updates

---
+++ precache-manifest.fdd2a329ba1a243b4f14373cf901678d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66d177d0059f118112e1c21c6ae78a3",
+    "url": "/GDL002-burger-queen/./index.html"
+  },
+  {
+    "revision": "c3d6120ad0f985b99763",
+    "url": "/GDL002-burger-queen/./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL002-burger-queen/./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c3d6120ad0f985b99763",
+    "url": "/GDL002-burger-queen/./static/js/main.05708121.chunk.js"
+  },
+  {
+    "revision": "5518386b4423cc379155",
+    "url": "/GDL002-burger-queen/./static/js/runtime~main.c68b016f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL002-burger-queen/./static/media/logo.5d5d9eef.svg"
+  }
+]);
